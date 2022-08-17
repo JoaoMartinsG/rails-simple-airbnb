64ded374cd9583492f2244d55655fb3e6452f3f2
joao martins <joaomgwork@hotmail.com>
2022-08-17 14:40:06 +0100

    first commit , still have to finish

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,29 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show edit]
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+end
